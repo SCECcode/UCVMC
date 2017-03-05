a551ff485e23f49029f664b8e951a52655616f17
Philip Maechling <pjmaechling@gmail.com>
2017-03-05 15:16:51 -0800

    Extend plots to show background model barriers.

--- utilities/make_plots.py
+++ utilities/make_plots.py
@@ -5,29 +5,31 @@ import os
 #
 # Make Cross Section Plots
 #
-cmd="./plot_cross_section.py -b 34.0,-118.75 -u 34.0,-117.5 -s 0 -e 500 -h 500 -v 5 -d vs -a d -c cvmh"
+cmd="./plot_cross_section.py -b 34.0,-122.00 -u 34.0,-117.5 -s 0 -e 2000 -h 500 -v 10 -d vs -a d -c cvmh"
 print cmd
 os.system(cmd)
 
-
-cmd="./plot_cross_section.py -b 34.0,-118.75 -u 34.0,-117.5 -s 0 -e 500 -h 500 -v 5 -d vs -a d -c cvms"
+cmd="./plot_cross_section.py -b 34.0,-122.00 -u 34.0,-117.5 -s 0 -e 2000 -h 500 -v 10 -d vs -a d -c cvms"
 print cmd
 os.system(cmd)
 
-
-cmd="./plot_cross_section.py -b 34.0,-118.75 -u 34.0,-117.5 -s 0 -e 500 -h 500 -v 5 -d vs -a d -c cvmsi"
+cmd="./plot_cross_section.py -b 34.0,-122.00 -u 34.0,-117.5 -s 0 -e 2000 -h 500 -v 10 -d vs -a d -c cvmsi"
 print cmd
 os.system(cmd)
 
-cmd="./plot_cross_section.py -b 34.0,-118.75 -u 34.0,-117.5 -s 0 -e 500 -h 500 -v 5 -d vs -a d -c cvms5"
+cmd="./plot_cross_section.py -b 34.0,-122.00 -u 34.0,-117.5 -s 0 -e 2000 -h 500 -v 10 -d vs -a d -c cvms5"
 print cmd
 os.system(cmd)
 
-cmd="./plot_cross_section.py -b 34.0,-118.75 -u 34.0,-117.5 -s 0 -e 500 -h 500 -v 5 -d vs -a d -c 1d"
+cmd="./plot_cross_section.py -b 34.0,-122.00 -u 34.0,-117.5 -s 0 -e 2000 -h 500 -v 10 -d vs -a d -c 1d"
 print cmd
 os.system(cmd)
 
-cmd="./plot_cross_section.py -b 34.0,-118.75 -u 34.0,-117.5 -s 0 -e 500 -h 500 -v 5 -d vs -a d -c bbp1d"
+cmd="./plot_cross_section.py -b 34.0,-122.00 -u 34.0,-117.5 -s 0 -e 2000 -h 500 -v 10 -d vs -a d -c bbp1d"
+print cmd
+os.system(cmd)
+
+cmd="./plot_cross_section.py -b 34.0,-122.00 -u 34.0,-117.5 -s 0 -e 2000 -h 500 -v 10 -d vs -a d -c cencal"
 print cmd
 os.system(cmd)
 
@@ -35,6 +37,34 @@ os.system(cmd)
 # Make Horizontal Slice Plots
 #
 cmd="./plot_horizontal_slice.py -b 33.5,-118.75 -u 34.5,-117.5 -s 0.1 -e 500 -d vs -a d -c cvmh"
+print cmd
+os.system(cmd)
+
+
+cmd="./plot_horizontal_slice.py -b 33.5,-118.75 -u 34.5,-117.5 -s 0.1 -e 500 -d vs -a d -c cvms"
+print cmd
+os.system(cmd)
+
+cmd="./plot_horizontal_slice.py -b 33.5,-118.75 -u 34.5,-117.5 -s 0.1 -e 500 -d vs -a d -c cvmsi"
+print cmd
+os.system(cmd)
+
+
+cmd="./plot_horizontal_slice.py -b 33.5,-118.75 -u 34.5,-117.5 -s 0.1 -e 500 -d vs -a d -c cvms5"
+print cmd
+os.system(cmd)
+
+
+cmd="./plot_horizontal_slice.py -b 33.5,-118.75 -u 34.5,-117.5 -s 0.1 -e 500 -d vs -a d -c 1d"
+print cmd
+os.system(cmd)
+
+
+cmd="./plot_horizontal_slice.py -b 33.5,-118.75 -u 34.5,-117.5 -s 0.1 -e 500 -d vs -a d -c bbp1d"
+print cmd
+os.system(cmd)
+
+cmd="./plot_horizontal_slice.py -b 33.5,-118.75 -u 34.5,-117.5 -s 0.1 -e 500 -d vs -a d -c cencal"
 print cmd
 os.system(cmd)
 
