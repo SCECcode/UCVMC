da3b5bacd41f0984cef1a6bf171c2910ab68c5ab
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2017-10-24 10:37:40 -0700

    fix the error message in utilities/pycvm so they match what is really wrong

--- utilities/pycvm/pycvm/common.py
+++ utilities/pycvm/pycvm/common.py
@@ -20,7 +20,7 @@ import getopt
 try:
     import numpy as np
 except StandardError, e:
-    print "ERROR: NumPy and Matplotlib must be installed on your system in order to generate these plots."
+    print "ERROR: NumPy must be installed on your system in order to generate these plots."
     exit(1)
     
 #  Matplotlib is required.
@@ -29,14 +29,14 @@ try:
     import matplotlib.colors as mcolors
     import matplotlib.cm as cm
 except StandardError, e:
-    print "ERROR: NumPy and Matplotlib must be installed on your system in order to generate these plots."
+    print "ERROR: Matplotlib must be installed on your system in order to generate these plots."
     exit(1)    
 
 #  Basemap is required.
 try:
     from mpl_toolkits import basemap
 except StandardError, e:
-    print "ERROR: Matplotlib Toolkit must be installed on your system in order to generate these plots."
+    print "ERROR: Basemap Toolkit must be installed on your system in order to generate these plots."
     exit(1)
 
 #  Constants
