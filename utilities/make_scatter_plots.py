cf7bd1014502e63be19e43bbefa26fdc62a8da20
Philip Maechling <pjmaechling@gmail.com>
2018-05-26 22:58:26 -0700

    added nafe_drake alrogithms in python for test plots

--- utilities/make_scatter_plots.py
+++ utilities/make_scatter_plots.py
@@ -7,7 +7,7 @@ import os
 cmd="./plot_map_grid.py -b 30.5,-126.0 -u 42.5,-112.5 -s 1.00 -e 0.0 -c cencal,cca,cvmsi  -o norcal_map_grid_0.txt"
 os.system(cmd)
 print cmd
-cmd="./plot_scatter_plot.py -i ./norcal_map_grid_0.txt  -e 0.0 -n norcal_map_grid_0_km -o nocal_vp_versus_density_0km.png"
+cmd="./plot_scatter_plot.py -i ./norcal_map_grid_0.txt  -e 0.0 -n \"CS18.4 Vp Density Scatter Plot 0k\" -o nocal_vp_versus_density_0km.png"
 os.system(cmd)
 print cmd
 
@@ -17,7 +17,7 @@ print cmd
 cmd="./plot_map_grid.py -b 30.5,-126.0 -u 42.5,-112.5 -s 1.00 -e 1000.0 -c cencal,cca,cvmsi  -o norcal_map_grid_1000.txt"
 os.system(cmd)
 print cmd
-cmd="./plot_scatter_plot.py -i ./norcal_map_grid_1000.txt  -e 1000.0 -n norcal_map_grid_0_km -o nocal_vp_versus_density_1000km.png"
+cmd="./plot_scatter_plot.py -i ./norcal_map_grid_1000.txt  -n \"CS18.5 Vp Density Scatter Plot 1000km\" -o nocal_vp_versus_density_1000km.png"
 os.system(cmd)
 print cmd
 
@@ -37,7 +37,7 @@ print cmd
 cmd="./plot_map_grid.py -b 30.5,-126.0 -u 42.5,-112.5 -s 1.00 -e 1000.0 -c cca  -o cca_map_grid_1000.txt"
 os.system(cmd)
 print cmd
-cmd="./plot_scatter_plot.py -i ./cca_map_grid_1000.txt  -e 1000.0 -n cca_map_grid_0_km -o cca_vp_versus_density_0km.png"
+cmd="./plot_scatter_plot.py -i ./cca_map_grid_1000.txt  -e 1000.0 -n cca_map_grid_0_km -o cca_vp_versus_density_1000km.png"
 os.system(cmd)
 print cmd
 
@@ -46,7 +46,7 @@ print cmd
 cmd="./plot_map_grid.py -b 30.5,-126.0 -u 42.5,-112.5 -s 1.00 -e 1000.0 -c cvms5  -o cvms5_map_grid_1000.txt"
 os.system(cmd)
 print cmd
-cmd="./plot_scatter_plot.py -i ./cvms5_map_grid_1000.txt  -e 1000.0 -n cvms5_map_grid_0_km -o cvms5_vp_versus_density_0km.png"
+cmd="./plot_scatter_plot.py -i ./cvms5_map_grid_1000.txt  -e 1000.0 -n cvms5_map_grid_0_km -o cvms5_vp_versus_density_1000km.png"
 os.system(cmd)
 print cmd
 
@@ -55,7 +55,7 @@ print cmd
 cmd="./plot_map_grid.py -b 30.5,-126.0 -u 42.5,-112.5 -s 1.00 -e 1000.0 -c cvmsi  -o cvmsi_map_grid_1000.txt"
 os.system(cmd)
 print cmd
-cmd="./plot_scatter_plot.py -i ./cvmsi_map_grid_1000.txt  -e 1000.0 -n cvmsi_map_grid_0_km -o cvmsi_vp_versus_density_0km.png"
+cmd="./plot_scatter_plot.py -i ./cvmsi_map_grid_1000.txt  -e 1000.0 -n cvmsi_map_grid_0_km -o cvmsi_vp_versus_density_1000km.png"
 os.system(cmd)
 print cmd
 
