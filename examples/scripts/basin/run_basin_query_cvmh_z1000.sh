64195c8fccce2b8f3477e1e4e2ce4f52e8ec4c01
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2019-04-02 16:02:22 -0700

    moved the basin related scripts back

--- examples/scripts/basin/run_basin_query_cvmh_z1000.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BIN_DIR=/home/scec-00/patrices/code/ucvm/src/basin
-
-${BIN_DIR}/basin_query -m cvmh -f ~/opt/aftershock/ucvm-12.2.0/conf/ucvm.conf -v 1000.0 < ./cvm_grid_locs_rev.txt > cvmh_z1000.txt
-
-
--- examples/scripts/basin/run_basin_query_cvmh_z2500.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BIN_DIR=/home/scec-00/patrices/code/ucvm/src/basin
-
-${BIN_DIR}/basin_query -m cvmh -f ~/opt/aftershock/ucvm-12.2.0/conf/ucvm.conf -v 2500.0 < ./cvm_grid_locs_rev.txt > cvmh_z2500.txt
-
-
