7ca62d38d829c9ac255ca35dbdd98b6fac1cb9bd
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2019-04-05 15:46:51 -0700

    add two example tests for basin_query_mpi and basin_query_mpi_complete

---
+++ examples/programs/basin/run_basin_query_mpi_complete_cencal_cvms5.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+##
+## example of running on usc/hpc cluster
+##
+
+## setup mpi environment
+source /usr/usc/openmpi/default/setup.sh
+
+BIN_DIR=${UCVM_INSTALL_PATH}/bin
+CONF_DIR=${UCVM_INSTALL_PATH}/conf
+
+salloc --ntasks=2 --time=00:10:00 srun --ntasks=2 -v --mpi=pmi2 ${BIN_DIR}/basin_query_mpi_complete -b hpc_cencal_cvms5_z2.5x.first,hpc_cencal_cvms5_z2.5x.firstOrSecond,hpc_cencal_cvms5_z2.5x.last,$hpc_cencal_cvms5_z2.5x.secondOnly,hpc_cencal_cvms5_z2.5x.threeLast -o hpc_cencal_cvms5_z2.5x.result,hpc_cencal_cvms5_z2.5x.meta.json -f ${CONF_DIR}/ucvm.conf -m cencal,cvms5 -i 20 -v 2500 -l 35,-122.5 -s 0.1 -x 16 -y 11 
+
+
+
