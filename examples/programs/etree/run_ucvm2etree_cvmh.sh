069ede49698d7e0f7c4051ad9a7d6eaa422482ce
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2019-04-06 12:41:57 -0700

    make ucvm2etree example work with right conf

--- examples/programs/etree/run_ucvm2etree_cvmh.sh
+++ examples/programs/etree/run_ucvm2etree_cvmh.sh
@@ -2,5 +2,7 @@
 BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 
+sed 's ${CONF_DIR} '$CONF_DIR' ' garnervalley_cvmh.conf_template > garnervalley_cvmh.conf
+
 ${BIN_DIR}/ucvm2etree -f ./garnervalley_cvmh.conf
 
