92705c5e1d63736d422387862c98df33c3ab779e
Mei-Hui Su <mei@MeiPro.local>
2019-04-06 23:19:26 -0700

    add more mesh examples

--- examples/programs/mesh/run_ucvm2mesh_mpi_layer_cvmsi.sh
+++ examples/programs/mesh/run_ucvm2mesh_mpi_layer_cvmsi.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/sh
+
+## setup mpi environment
+source /usr/usc/openmpi/default/setup.sh
+
 BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 SCRATCH=./scratch
 
 sed 's ${CONF_DIR} '$CONF_DIR' ' la_habra_cvmsi.conf_template | sed 's ${SCRATCH} '$SCRATCH' ' > la_habra_cvmsi.conf
 
-#${BIN_DIR}/ucvm2mesh_mpi_layer -f la_habra_cvmsi.conf -l 1 -c 3
-#${BIN_DIR}/ucvm2mesh_mpi_layer -f la_habra_cvmsi.conf -l 4 -c 3 
-#${BIN_DIR}/ucvm2mesh_mpi_layer -f la_habra_cvmsi.conf -l 7 -c 4 
+salloc --ntasks=4 --time=00:10:00 srun --ntasks=4 -v --mpi=pmi2 ${BIN_DIR}/ucvm2mesh_mpi_layer -f la_habra_cvmsi.conf -l 1 -c 3
+salloc --ntasks=4 --time=00:10:00 srun --ntasks=4 -v --mpi=pmi2 ${BIN_DIR}/ucvm2mesh_mpi_layer -f la_habra_cvmsi.conf -l 4 -c 3 
+salloc --ntasks=4 --time=00:10:00 srun --ntasks=4 -v --mpi=pmi2 ${BIN_DIR}/ucvm2mesh_mpi_layer -f la_habra_cvmsi.conf -l 7 -c 4 
 
 
