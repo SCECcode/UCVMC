9c0609cc8a1c469b7b4881397ba6be35902d21f8
Mei-Hui Su <mei@MeiPro.local>
2019-04-06 21:33:45 -0700

    add small ucvm2mesh example

---
+++ examples/programs/mesh/run_ucvm2mesh_cvmh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+BIN_DIR=${UCVM_INSTALL_PATH}/bin
+CONF_DIR=${UCVM_INSTALL_PATH}/conf
+SCRATCH=./scratch
+
+sed 's ${CONF_DIR} '$CONF_DIR' ' small_cvmh.conf_template | sed 's ${SCRATCH} '$SCRATCH' ' > small_cvmh.conf
+
+#${BIN_DIR}/ucvm2mesh -f small_cvmh.conf_template
+
+
