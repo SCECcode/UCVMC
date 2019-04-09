6298e6721ea11e0770f97072ecc5b8b51361384f
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2019-04-09 14:52:25 -0700

    add check on mpi for hpc specific examples

--- examples/programs/basin/run_basin_query_cvmh.sh
+++ examples/programs/basin/run_basin_query_cvmh.sh
@@ -12,4 +12,4 @@ MODEL=cvmh
 
 ${BIN_DIR}/basin_query -m ${MODEL} -f ${CONF_DIR}/ucvm.conf -v 2500.0 < ${TEST_DIR}/test_latlonsonly.txt > cvmh_z2500_basin_query.txt
 
-${BIN_DIR}/basin_query -m ${MODEL} -f ${CONF_DIR}/ucvm.conf -v 1000.0 < ${TEST_DIR}/test_latlonsonly.txt > cvmh_1000_basin_query.txt
+${BIN_DIR}/basin_query -m ${MODEL} -f ${CONF_DIR}/ucvm.conf -v 1000.0 < ${TEST_DIR}/test_latlonsonly.txt > cvmh_z1000_basin_query.txt
