c8179dda614541aff8566d8b28f0220ff3fa585d
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2019-04-23 09:08:57 -0700

    fix examples to change from - to _ in program name

--- examples/programs/mesh/run_ucvm2mesh_mpi_cvmh.sh
+++ examples/programs/mesh/run_ucvm2mesh_mpi_cvmh.sh
@@ -13,18 +13,18 @@ fi
 ## setup mpi environment
 source /usr/usc/openmpi/default/setup.sh
 
-rm -rf ucvm2mesh-mpi ucvm.conf small_cvmh.conf
+rm -rf ucvm2mesh_mpi ucvm.conf small_cvmh.conf
 
 BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 SCRATCH=./scratch
 
-cp ${BIN_DIR}/ucvm2mesh-mpi .
+cp ${BIN_DIR}/ucvm2mesh_mpi .
 cp ${CONF_DIR}/ucvm.conf .
 
 sed 's ${SCRATCH} '$SCRATCH' ' small_cvmh.conf_template  > small_cvmh.conf
 
-salloc -N 2 --ntasks=2 --time=00:20:00 srun --ntasks=2 -v --mpi=pmi2 ./ucvm2mesh-mpi -f small_cvmh.conf
+salloc -N 2 --ntasks=2 --time=00:20:00 srun --ntasks=2 -v --mpi=pmi2 ./ucvm2mesh_mpi -f small_cvmh.conf
 
 
 
--- examples/programs/mesh/run_ucvm2mesh_mpi_cvmsi.sh
+++ examples/programs/mesh/run_ucvm2mesh_mpi_cvmsi.sh
@@ -13,17 +13,17 @@ fi
 ## setup mpi environment
 source /usr/usc/openmpi/default/setup.sh
 
-rm -rf ucvm2mesh-mpi ucvm.conf small-cvmsi.conf small_cvmsi.grid small_cvmsi.media
+rm -rf ucvm2mesh_mpi ucvm.conf small-cvmsi.conf small_cvmsi.grid small_cvmsi.media
 
 BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 SCRATCH=./scratch
 
-cp ${BIN_DIR}/ucvm2mesh-mpi .
+cp ${BIN_DIR}/ucvm2mesh_mpi .
 cp ${CONF_DIR}/ucvm.conf .
 
 sed 's ${SCRATCH} '$SCRATCH' ' small_cvmsi.conf_template > small_cvmsi.conf
 
-salloc -N 4 --ntasks=8 --time=00:30:00 srun --ntasks=8 -v --mpi=pmi2 ${BIN_DIR}/ucvm2mesh-mpi -f small_cvmsi.conf
+salloc -N 4 --ntasks=8 --time=00:30:00 srun --ntasks=8 -v --mpi=pmi2 ${BIN_DIR}/ucvm2mesh_mpi -f small_cvmsi.conf
 
 
