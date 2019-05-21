ea9b7275fbef86b309de007dd308211771e489cc
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2019-05-21 14:36:12 -0700

    lost the ${CONF_DIR}/ from examples/programs/mesh/*template

--- examples/programs/mesh/run_ucvm2mesh_cvmh.sh
+++ examples/programs/mesh/run_ucvm2mesh_cvmh.sh
@@ -9,8 +9,12 @@ BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 SCRATCH=./scratch
 
-sed 's ${CONF_DIR} '$CONF_DIR' ' small_cvmh.conf_template | sed 's ${SCRATCH} '$SCRATCH' ' > small_cvmh.conf
+echo $CONF_DIR
 
-${BIN_DIR}/ucvm2mesh -f small_cvmh.conf
+sed 's ${CONF_DIR} '$CONF_DIR' ' small_cvmh.conf_template 
+
+#sed 's ${CONF_DIR} '$CONF_DIR' ' small_cvmh.conf_template | sed 's ${SCRATCH} '$SCRATCH' ' > small_cvmh.conf
+
+#${BIN_DIR}/ucvm2mesh -f small_cvmh.conf
 
 
