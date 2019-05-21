15f35ea93e47f57ec8532c871b38e68431090395
Mei-Hui Su <mei@hpc-login3.hpcc.usc.edu>
2019-05-21 14:38:20 -0700

    fix the test

--- examples/programs/mesh/run_ucvm2mesh_cvmh.sh
+++ examples/programs/mesh/run_ucvm2mesh_cvmh.sh
@@ -9,12 +9,8 @@ BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 SCRATCH=./scratch
 
-echo $CONF_DIR
+sed 's ${CONF_DIR} '$CONF_DIR' ' small_cvmh.conf_template | sed 's ${SCRATCH} '$SCRATCH' ' > small_cvmh.conf
 
-sed 's ${CONF_DIR} '$CONF_DIR' ' small_cvmh.conf_template 
-
-#sed 's ${CONF_DIR} '$CONF_DIR' ' small_cvmh.conf_template | sed 's ${SCRATCH} '$SCRATCH' ' > small_cvmh.conf
-
-#${BIN_DIR}/ucvm2mesh -f small_cvmh.conf
+${BIN_DIR}/ucvm2mesh -f small_cvmh.conf
 
 
