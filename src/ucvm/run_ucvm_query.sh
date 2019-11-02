8a994291715fed17a7a73f1b2b4352e0d537ad8d
root <root@ip-172-31-31-151.us-west-2.compute.internal>
2019-11-02 06:53:30 +0000

    cvmsi(cvms426) has .so

--- src/ucvm/run_ucvm_query.sh
+++ src/ucvm/run_ucvm_query.sh
@@ -20,8 +20,9 @@ shift $(($OPTIND - 1))
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-export LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib/euclid3/lib:${SCRIPT_DIR}/../lib/proj-5/lib
-export DYLD_LIBRARY_PATH=${SCRIPT_DIR}/../lib/euclid3/lib:${SCRIPT_DIR}/../lib/proj-5/lib
+## need to add path to cvmsi(cvms426)
+export LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib/euclid3/lib:${SCRIPT_DIR}/../lib/proj-5/lib:${SCRIPT_DIR}/../model/cvms426/lib
+export DYLD_LIBRARY_PATH=${SCRIPT_DIR}/../lib/euclid3/lib:${SCRIPT_DIR}/../lib/proj-5/lib:${SCRIPT_DIR}/../model/cvms426/lib
 
 ${SCRIPT_DIR}/ucvm_query ${FLAGS} 
 
