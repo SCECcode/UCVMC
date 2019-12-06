55e48dab506204fd8dcd5506bb60027e5c774596
Mei-Hui Su <mei@MeiPro.local>
2019-12-06 00:55:48 -0800

    add flags to pickup STDERR STDOUT file to be used with run_ucvm_query.sh

--- src/ucvm/run_ucvm_query.sh
+++ src/ucvm/run_ucvm_query.sh
@@ -8,9 +8,13 @@
 FLAGS=""
 
 # Pass along any arguments to UCVM
-while getopts 'm:p:c:f:z:bl:' OPTION
+while getopts 'm:p:c:f:z:bl:I:O:' OPTION
 do
-  if [ "$OPTARG" != "" ]; then
+  if [ "$OPTION" == "O" ]; then
+      OUT=$OPTARG
+  elif [ "$OPTION" == "I" ]; then
+      IN=$OPTARG
+  elif [ "$OPTARG" != "" ]; then
       FLAGS="${FLAGS} -$OPTION $OPTARG"
   else
       FLAGS="${FLAGS} -$OPTION"
@@ -24,5 +28,13 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 export LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib/euclid3/lib:${SCRIPT_DIR}/../lib/proj-5/lib:${SCRIPT_DIR}/../model/cvms426/lib
 export DYLD_LIBRARY_PATH=${SCRIPT_DIR}/../lib/euclid3/lib:${SCRIPT_DIR}/../lib/proj-5/lib:${SCRIPT_DIR}/../model/cvms426/lib
 
-${SCRIPT_DIR}/ucvm_query ${FLAGS} 
+if [ "$IN" != "" ]; then
+  if [ "$OUT" != "" ]; then
+      ${SCRIPT_DIR}/ucvm_query ${FLAGS} < $IN 1 >> $OUT 2>/dev/null 
+  else
+      ${SCRIPT_DIR}/ucvm_query ${FLAGS} < $IN
+  fi
+else
+      ${SCRIPT_DIR}/ucvm_query ${FLAGS} 
+fi
 
