bfb8a31081e4c15d4dba93e45b328f156453dced
Mei-Hui Su <mei@MeiPro.local>
2020-03-30 09:54:40 -0700

    expand run_ucvm_query.sh's handling of -f option

--- src/ucvm/run_ucvm_query.sh
+++ src/ucvm/run_ucvm_query.sh
@@ -6,6 +6,7 @@
 
 # Process options
 FLAGS=""
+CONF=""
 
 # Pass along any arguments to UCVM
 while getopts 'm:p:c:f:z:bl:I:O:' OPTION
@@ -14,6 +15,9 @@ do
       OUT=$OPTARG
   elif [ "$OPTION" == "I" ]; then
       IN=$OPTARG
+  elif [ "$OPTION" == "f" ]; then
+      CONF=$OPTARG
+      FLAGS="${FLAGS} -$OPTION $OPTARG"
   elif [ "$OPTARG" != "" ]; then
       FLAGS="${FLAGS} -$OPTION $OPTARG"
   else
@@ -28,6 +32,16 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 export LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib/euclid3/lib:${SCRIPT_DIR}/../lib/proj-5/lib:${SCRIPT_DIR}/../model/cvms426/lib:${SCRIPT_DIR}/../model/cvms5/lib:${SCRIPT_DIR}/../model/cca/lib:${SCRIPT_DIR}/../model/cencal/lib:${SCRIPT_DIR}/../model/cs173/lib
 
 export DYLD_LIBRARY_PATH=${SCRIPT_DIR}/../lib/euclid3/lib:${SCRIPT_DIR}/../lib/proj-5/lib:${SCRIPT_DIR}/../model/cvms426/lib:${SCRIPT_DIR}/../model/cvms5/lib:${SCRIPT_DIR}/../model/cca/lib:${SCRIPT_DIR}/../model/cencal/lib:${SCRIPT_DIR}/../model/cs173/lib
+
+if [ "$CONF" == "" ]; then
+   if [ -f ./ucvm_conf  ]; then 
+      FLAGS="${FLAGS} -f ./ucvm_conf"
+   else
+      if [ -z "${UCVM_INSTALL_PATH}" ]; then
+          FLAGS="${FLAGS} -f ${UCVM_INSTALL_PATH}
+      fi 
+   fi
+fi
 
 if [ "$IN" != "" ]; then
   if [ "$OUT" != "" ]; then
