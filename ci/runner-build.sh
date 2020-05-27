0270eb6949c048e302bdecdc4d204ffc3e9d9d7a
Mei-Hui Su <mei@MeiPro.local>
2020-05-27 15:15:52 -0700

    tweak

--- ci/runner-build.sh
+++ ci/runner-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+pwd=`pwd`
+export UCVM_SRC_PATH=$pwd
+export UCVM_INSTALL_PATH=$pwd/TARGET_UCVMC
+export LD_LIBRARY_PATH=$UCVM_INSTALL_PATH/lib/euclid3/lib:$UCVM_INSTALL_PATH/lib/proj-5/lib:$UCVM_INSTALL_PATH/model/cvms/lib:$UCVM_INSTALL_PATH/model/cvms5/lib:$UCVM_INSTALL_PATH/model/cvmh1511/lib 
+
 mkdir $UCVM_INSTALL_PATH
 
 cd $UCVM_SRC_PATH/largefiles
