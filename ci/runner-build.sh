48b2f7a8b9c26095299cc9e7b960a7e3d74b6ceb
Mei-Hui Su <mei@MeiPro.local>
2020-05-27 16:16:27 -0700

    add gfortran

--- ci/runner-build.sh
+++ ci/runner-build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+sudo apt-get install gfortran
+
 pwd=`pwd`
 export UCVM_SRC_PATH=$pwd
 export UCVM_INSTALL_PATH=$pwd/TARGET_UCVMC
 export LD_LIBRARY_PATH=$UCVM_INSTALL_PATH/lib/euclid3/lib:$UCVM_INSTALL_PATH/lib/proj-5/lib:$UCVM_INSTALL_PATH/model/cvms/lib:$UCVM_INSTALL_PATH/model/cvms5/lib:$UCVM_INSTALL_PATH/model/cvmh1511/lib 
 
-env
 
 mkdir $UCVM_INSTALL_PATH
 
