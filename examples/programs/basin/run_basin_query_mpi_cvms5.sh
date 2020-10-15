51f7585f74098c38b100573ce6520462be408404
Mei-Hui Su <mei@MeiPro.local>
2020-10-15 11:40:54 -0700

    add self check to an mpi script

--- examples/programs/basin/run_basin_query_mpi_cvms5.sh
+++ examples/programs/basin/run_basin_query_mpi_cvms5.sh
@@ -17,8 +17,33 @@ BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 TEST=basin_query_mpi_cvms5
 
+expect=$(mktemp) || exit 1
+result=$(mktemp) || (trap 'rm -f "$expect"'; exit 1)
+
 salloc --ntasks=2 --time=00:10:00 srun --ntasks=2 -o ${TEST}.srun.out -v ${BIN_DIR}/basin_query_mpi -b ${TEST}.simple -f ${CONF_DIR}/ucvm.conf -m cvms5 -i 20 -v 2500 -l 35.0,-122.5 -s 0.1 -x 16 -y 11
 
+od ${TEST}.simple |tail -10 > $result 2>&1
+
+cat > $expect << EOF_EXPECTED_RESULT
+*
+0001040 000000 137600 000000 137600 000000 137600 000000 000000
+0001060 000000 000000 000000 000000 000000 000000 000000 000000
+0001100 000000 137600 000000 137600 000000 137600 000000 137600
+*
+0001160 000000 000000 000000 000000 000000 000000 000000 000000
+0001200 000000 137600 000000 137600 000000 137600 000000 137600
+*
+0001260 000000 137600 000000 000000 000000 000000 000000 000000
+0001300
+EOF_EXPECTED_RESULT
+
+if diff $result $expect > /dev/null 2>&1
+then
+  echo [SUCCESS]
+else
+  echo [FAILURE]
+fi
+                               
 
 
 
