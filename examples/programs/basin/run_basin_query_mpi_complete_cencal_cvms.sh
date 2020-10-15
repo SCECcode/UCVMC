25ba2e4b7988a5154bdda2d09d92d0c7359c0105
Mei-Hui Su <mei@MeiPro.local>
2020-10-15 15:42:51 -0700

    make salloc really quiet

--- examples/programs/basin/run_basin_query_mpi_complete_cencal_cvms.sh
+++ examples/programs/basin/run_basin_query_mpi_complete_cencal_cvms.sh
@@ -17,7 +17,7 @@ BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 TEST=basin_query_mpi_complete_cencal_cvms_z2.5x
 
-salloc --ntasks=2 --time=00:10:00 srun -Q --ntasks=2 -o ${TEST}.srun.out ${BIN_DIR}/basin_query_mpi_complete -b ${TEST}.first,${TEST}.firstOrSecond,${TEST}.last,${TEST}.secondOnly,${TEST}.threeLast -o ${TEST}.result,${TEST}.meta.json -f ${CONF_DIR}/ucvm.conf -m cencal,cvms -i 20 -v 2500 -l 35,-122.5 -s 0.1 -x 16 -y 11 
+salloc -Q --ntasks=2 --time=00:10:00 srun -Q --ntasks=2 -o ${TEST}.srun.out ${BIN_DIR}/basin_query_mpi_complete -b ${TEST}.first,${TEST}.firstOrSecond,${TEST}.last,${TEST}.secondOnly,${TEST}.threeLast -o ${TEST}.result,${TEST}.meta.json -f ${CONF_DIR}/ucvm.conf -m cencal,cvms -i 20 -v 2500 -l 35,-122.5 -s 0.1 -x 16 -y 11 
 
 expect=$(mktemp) || exit 1
 result=$(mktemp) || (trap 'rm -f "$expect"'; exit 1)
