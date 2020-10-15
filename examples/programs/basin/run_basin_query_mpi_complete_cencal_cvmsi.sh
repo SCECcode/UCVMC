57ea66a1a7dd98b3528e58178ccf5c8798f0a190
Mei-Hui Su <mei@MeiPro.local>
2020-10-15 15:12:06 -0700

    add more self test

--- examples/programs/basin/run_basin_query_mpi_complete_cencal_cvmsi.sh
+++ examples/programs/basin/run_basin_query_mpi_complete_cencal_cvmsi.sh
@@ -17,7 +17,7 @@ BIN_DIR=${UCVM_INSTALL_PATH}/bin
 CONF_DIR=${UCVM_INSTALL_PATH}/conf
 TEST=basin_query_mpi_complete_cencal_cvmsi_z2.5x
 
-salloc --ntasks=2 --time=00:10:00 srun --ntasks=2 -o ${TEST}.srun.out -v ${BIN_DIR}/basin_query_mpi_complete -b ${TEST}.first,${TEST}.firstOrSecond,${TEST}.last,${TEST}.secondOnly,${TEST}.threeLast -o ${TEST}.result,${TEST}.meta.json -f ${CONF_DIR}/ucvm.conf -m cencal,cvmsi -i 20 -v 2500 -l 35,-122.5 -s 0.1 -x 16 -y 11 
+salloc --ntasks=2 --time=00:10:00 srun -Q --ntasks=2 -o ${TEST}.srun.out -v ${BIN_DIR}/basin_query_mpi_complete -b ${TEST}.first,${TEST}.firstOrSecond,${TEST}.last,${TEST}.secondOnly,${TEST}.threeLast -o ${TEST}.result,${TEST}.meta.json -f ${CONF_DIR}/ucvm.conf -m cencal,cvmsi -i 20 -v 2500 -l 35,-122.5 -s 0.1 -x 16 -y 11 
 
 expect=$(mktemp) || exit 1
 result=$(mktemp) || (trap 'rm -f "$expect"'; exit 1)
