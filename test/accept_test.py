17a04fabc3362dd591614ff93336a689f9af07f5
Mei-Hui Su <mei@MeiPro.local>
2021-01-15 13:36:22 -0800

    adjust vs30_query output because of new 'method' of computing the vs30 values

--- test/accept_test.py
+++ test/accept_test.py
@@ -19,7 +19,8 @@ def test_vs30_query(dir):
                   "-i", "0.1"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     output = proc.communicate(input=b"-118 34\n-117 35")[0]
     
-    expected_output = b" -118.0000    34.0000    777.764\n -117.0000    35.0000    777.764\n"
+## OLD one    expected_output = b" -118.0000    34.0000    777.764\n -117.0000    35.0000    777.764\n"
+    expected_output = b" -118.0000    34.0000    399.698\n -117.0000    35.0000    399.698\n"
     
     if not output == expected_output:
         print("Error: Vs30 expected output not equal to actual output.\n")
