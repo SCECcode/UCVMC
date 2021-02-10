fc5b26bf2d6333d1e9c3ca227fa045acae10201c
Mei-Hui Su <mei@MeiPro.local>
2021-02-10 00:28:55 -0800

    put exit code to ucvm-test.sh

--- .github/scripts/ucvm-test.sh
+++ .github/scripts/ucvm-test.sh
@@ -2,4 +2,14 @@
 
 source $UCVM_INSTALL_PATH/conf/ucvm_env.sh
 cd $UCVM_INSTALL_PATH/tests
-./run-testing
+./run-testing | tee result.txt
+
+p=`grep -c FAIL result.txt` 
+
+if [ $p != 0 ]; then
+   echo "something wrong.."
+   exit 1 
+else
+   echo "okay"
+   exit 0
+fi
